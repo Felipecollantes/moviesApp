26c6c25020ada10f09d2a54f31d416cbf7502b2f
FelipePCollantes <fpitavc@eservicios.indra.es>
2021-09-11 11:57:45 +0200

    [FPC] getFeature created

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class MoviesService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  getFeature(){
+    // eslint-disable-next-line max-len
+    return this.http.get('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-01-31&api_key=ea095bf9e7e0831f4c5c72719e344d66&language=es&include_image_language=es');
+  }
 }
