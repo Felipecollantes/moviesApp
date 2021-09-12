c9e9e5759913dcbb2c9d74387bb4415cc5132d28
FelipePCollantes <fpitavc@eservicios.indra.es>
2021-09-12 12:17:39 +0200

    [FPC] creacion buscar pelicula

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  textoBuscar: string;
+  ideas: string[];
 
-  constructor() {}
+  constructor() {
+    this.textoBuscar = '';
+    this.ideas = [
+      'Spiderman',
+      'Avenger',
+      'El señor de los anillos',
+      'El Padrino',
+    ];
+  }
 
+  buscar(event) {
+    const valor = event.detail.value;
+    console.log(valor);
+  }
 }
